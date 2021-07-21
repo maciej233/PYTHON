0148a1bbe2b7001787df7c359bf1382adfe4deeb
maciej <m.sobiech@gmail.com>
2021-07-21 13:20:25 +0200

    new files

--- scapy/check_open_port.py
+++ scapy/check_open_port.py
@@ -1,17 +1,22 @@
 from scapy.all import *
+
 import sys
 
-def tcp_scan(IP, dport):
-    #ans, uans = 
-    pass
-
-
+def tcp_scan(ip, dport):
+    ans, _ = sr(IP(dst=ip)/TCP(dport=dport, sport=666, flags="S"))
+    for _, returned  in ans:
+        if "SA" in str(returned[TCP].flags):
+            return f"for device ip {ip} port {dport} is open"
+        else:
+            return f"for device ip {ip} port {dport} is CLOSED"
+    
 
 def main():
-    destination = sys.argv[1]
+    ip = sys.argv[1]
     dport = int(sys.argv[2])
-    scan_result = tcp_scan(destination, dport)
-    print(scan_result)
+    output = tcp_scan(ip, dport)
+    print(output)
+
 
 if __name__ == "__main__":
     main()
