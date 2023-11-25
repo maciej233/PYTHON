c5fb24aa8beabe06d2d2837d3c8fda525fc3193c
maciej233 <m.sobiech.wat@gmail.com>
2023-11-25 16:35:39 +0100

    done

--- small_helpers/yt_download.py
+++ small_helpers/yt_download.py
@@ -1,1 +1,10 @@
-from yt_download import *
+from pytube import YouTube
+import sys
+
+link = sys.argv[1]
+
+yt = YouTube(link)
+
+yd = yt.streams.get_highest_resolution()
+yd.download("/home/vncuser1/videos")
+print(f"Name:{yt.title}")
